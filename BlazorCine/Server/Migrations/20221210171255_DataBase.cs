6d1f485f5a91b544e84a776b02d82653499e0222
Jose Carlos <rodjosecarlos15@gmail.com>
2022-12-10 10:34:19 -0700

    Se agrego el UseSqlServer en Program y se hizo lamigracion y el update

---
+++ BlazorCine/Server/Migrations/20221210171255_DataBase.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorCine.Server.Migrations
+{
+    public partial class DataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carteleras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Horario = table.Column<int>(type: "int", nullable: false),
+                    Sala = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carteleras", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Horarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Hora = table.Column<int>(type: "int", nullable: false),
+                    CarteleraId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Horarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Horarios_Carteleras_CarteleraId",
+                        column: x => x.CarteleraId,
+                        principalTable: "Carteleras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Peliculas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Duracion = table.Column<int>(type: "int", nullable: false),
+                    Idioma = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CarteleraId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Peliculas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Peliculas_Carteleras_CarteleraId",
+                        column: x => x.CarteleraId,
+                        principalTable: "Carteleras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TipoSala = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrecioSala = table.Column<int>(type: "int", nullable: false),
+                    CarteleraId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Salas_Carteleras_CarteleraId",
+                        column: x => x.CarteleraId,
+                        principalTable: "Carteleras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Horarios_CarteleraId",
+                table: "Horarios",
+                column: "CarteleraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Peliculas_CarteleraId",
+                table: "Peliculas",
+                column: "CarteleraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Salas_CarteleraId",
+                table: "Salas",
+                column: "CarteleraId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Horarios");
+
+            migrationBuilder.DropTable(
+                name: "Peliculas");
+
+            migrationBuilder.DropTable(
+                name: "Salas");
+
+            migrationBuilder.DropTable(
+                name: "Carteleras");
+        }
+    }
+}
